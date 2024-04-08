05f1a7d8fa4517ca6ca382ddb099b27a95464a34
Ashish B <cameron.ashish@gmail.com>
2024-04-08 11:27:21 +0530

    Add Bootstrap, Navbaar component, and update index.html and index.js
    This commit adds Bootstrap 5.3.3 to the project, includes a new Navbaar component, and updates index.html to load Bootstrap CSS and JS, and index.js to import Bootstrap CSS.

---
+++ src/components/Navbaar.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { Navbar, Container, Button, Dropdown } from 'react-bootstrap';
+import { FaUser } from 'react-icons/fa'; 
+
+
+const Navbaar = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLoginClick = () => {
+        setIsLoggedIn(true);
+    };
+
+    const handleLogoutClick = () => {
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <Navbar expand="lg" bg="light" variant="dark" sticky="top">
+            <Container>
+                <Navbar.Brand href="#home" className='text-dark'>
+                    <img
+                        alt=""
+                        src="" 
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />
+                    Company
+                </Navbar.Brand>
+                <div className="ms-auto">
+                    {isLoggedIn ? (
+                        <Dropdown align="end">
+                            <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components" className='text-dark'>
+                                <FaUser />
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu className="dropdown-menu-end">
+                                <Dropdown.Item href="#profile">My Profile</Dropdown.Item>
+                                <Dropdown.Item href="#bookings">My Bookings</Dropdown.Item>
+                                <Dropdown.Item href="#cars">My Cars</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item href="#logout" onClick={handleLogoutClick}>Logout</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    ) : (
+                        <Button variant="outline-primary" onClick={handleLoginClick}>Login/Signup</Button>
+                    )}
+                </div>
+            </Container>
+        </Navbar>
+    );
+}
+
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const handleToggleClick = (e) => {
+        e.preventDefault();
+        setIsDropdownOpen(!isDropdownOpen);
+        onClick(e);
+    };
+
+    return (
+        <Button
+            ref={ref}
+            onClick={handleToggleClick}
+            className={`btn-circle ${isDropdownOpen ? 'dropdown show' : 'dropdown'}`}
+        >
+            {children} 
+        </Button>
+
+    );
+});
+
+
+export default Navbaar;
